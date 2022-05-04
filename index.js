18495eac55ec326efeab2b0671218f4161ec8396
Dr.Kh.Shoaib Ibne Zaman Pial <ismataraeva2712>
2022-05-04 19:51:52 +0600

    add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -10,6 +11,21 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+function verifyJwt(req,res,next){
+    const authHeader=req.headers.authorization 
+    if(!authHeader){
+        return res.status(401).send({message:'unauthorizes access'})
+    }
+    const token=authHeader.split(' ')[1]
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+            return res.status(403).send({message:'forbidden access'})
+            console.log('decoded',decoded)
+        }
+    })
+   
+    next()
+}
 
 // connected database
 // =======================
@@ -23,8 +39,15 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        const serviceCollection = client.db("genius-car").collection("service")
-
+        const serviceCollection = client.db('genius-car').collection('service')
+        const orderCollection = client.db('genius-car').collection('order')
+        // Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+            res.send({ accessToken })
+        })
+        // service
         app.get('/service', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
@@ -54,6 +77,21 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await serviceCollection.deleteOne(query);
+            res.send(result)
+        })
+        // order collection Api 
+        app.get('/order',verifyJwt, async (req, res) => {
+            
+            const email = req.query.email;
+            console.log(email)
+            const query = { email: email }
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray()
+            res.send(orders)
+        })
+        app.post('/order', async (req, res) => {
+            const order = req.body
+            const result = await orderCollection.insertOne(order)
             res.send(result)
         })
     }
